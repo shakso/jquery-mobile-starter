1a09f292743296d233b6db0a0b9786a1dbf79797
Alex Shakespeare <alex@shakso.com>
2014-11-01 19:47:39 +0000

    more

--- js/app/app.js
+++ js/app/app.js
@@ -7,7 +7,6 @@ $(function() {
   		stop: function( event, ui ) {
   			console.log();
   			$.ajax({url:"http://megabox/switch.html?device=" + event.target.attributes.getNamedItem("device").value + "&cmd=" + event.target.value,success:function(result){
-    			//$("#div1").html(result);
   			}});
   		}
 	});
@@ -19,17 +18,24 @@ $(function() {
 
 
 	function Refresh() {
-		$.ajax({url:"http://megabox/switch.html?cmd=status",success:function(result){
-		var statuses = JSON.parse(result);
-		$('select').each(function(device) { 
-			if (statuses[$(this)[0].attributes.getNamedItem("device").value]) {
-				$(this).val(statuses[$(this)[0].attributes.getNamedItem("device").value]);
-				$(this).slider('refresh');
-				$(this).slider({ disabled: false });
-			} else {
-				$(this).slider({ disabled: true });
+		$.ajax({url:"http://megabox/switch.html?cmd=status",
+			success:function(result){
+				var statuses = JSON.parse(result);
+				$('select').each(function(device) { 
+					if (statuses[$(this)[0].attributes.getNamedItem("device").value]) {
+						$(this).val(statuses[$(this)[0].attributes.getNamedItem("device").value]);
+						$(this).slider('refresh');
+						$(this).slider({ disabled: false });
+					} else {
+						$(this).slider({ disabled: true });
+					}
+				})
+			},
+			error:function(){
+				$('select').each(function(device) { 
+					$(this).slider({ disabled: true });
+				})
 			}
-		});
-  		}});
+  		});
 	}
 });
