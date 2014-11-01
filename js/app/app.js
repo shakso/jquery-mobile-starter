8f95ffd6df8d8152023f506204530a175d1b4c55
Alex Shakespeare <alex@shakso.com>
2014-11-01 18:42:38 +0000

    more

--- js/app/app.js
+++ js/app/app.js
@@ -5,10 +5,31 @@ var startApp = function() {
 $(function() {
 	$( ".slider" ).slider({
   		stop: function( event, ui ) {
-  			console.log(event.target.value);
-  			$.ajax({url:"http://megabox/switch.html?state=" + event.target.value,success:function(result){
+  			console.log();
+  			$.ajax({url:"http://megabox/switch.html?device=" + event.target.attributes.getNamedItem("device").value + "&cmd=" + event.target.value,success:function(result){
     			//$("#div1").html(result);
   			}});
   		}
 	});
+
+
+	setInterval(function() { Refresh(); }, 2000);
+	Refresh();
+
+
+
+	function Refresh() {
+		$.ajax({url:"http://megabox/switch.html?cmd=status",success:function(result){
+		var statuses = JSON.parse(result);
+		$('select').each(function(device) { 
+			if (statuses[$(this)[0].attributes.getNamedItem("device").value]) {
+				$(this).val(statuses[$(this)[0].attributes.getNamedItem("device").value]);
+				$(this).slider('refresh');
+				$(this).slider({ disabled: false });
+			} else {
+				$(this).slider({ disabled: true });
+			}
+		});
+  		}});
+	}
 });
