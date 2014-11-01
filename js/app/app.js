7090ce5abf562353aaedbdd5a00b64faedd65172
Alex Shakespeare <alex@shakso.com>
2014-11-01 15:52:33 +0000

    Initial

--- js/app/app.js
+++ js/app/app.js
@@ -1,3 +1,14 @@
 var startApp = function() {
-  // alert('started');
 };
+
+
+$(function() {
+	$( "#slider" ).slider({
+  		stop: function( event, ui ) {
+  			console.log(event.target.value);
+  			$.ajax({url:"http://megabox/switch.html?state=" + event.target.value,success:function(result){
+    			//$("#div1").html(result);
+  			}});
+  		}
+	});
+});
