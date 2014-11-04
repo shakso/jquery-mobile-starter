0f5c53a9b941a90f26f0a8e26e4076a37c6c6492
Alex Shakespeare <alex@shakso.com>
2014-11-04 20:25:04 +0000

    websocket yay

--- js/app/app.js
+++ js/app/app.js
@@ -3,41 +3,43 @@ var startApp = function() {
 
 
 $(function() {
+
+	var socket = new WebSocket("ws://megabox:3000");
+	socket.onmessage = function(msg){
+		var data=JSON.parse(msg.data);
+		if (data.hosts) {
+			var hosts=[];
+			for (var i=0; i<data.hosts.length; i++) { 
+				hosts[data.hosts[i].name]=data.hosts[i];
+			}
+			$('select').each(function(device) { 
+				var devicename=$(this)[0].attributes.getNamedItem("device").value;
+				if (hosts[devicename]) {
+					if (hosts[devicename].state == true) {
+						$(this).val('on');
+					} else {
+						$(this).val('off');	
+					}
+					$(this).slider('refresh');
+					$(this).slider({ disabled: false });
+				} else {
+					$(this).slider({ disabled: true });
+				}
+			})
+		}
+	}
+	socket.onopen = function(){
+   		socket.send('discover');
+	} 
+
 	$( ".slider" ).slider({
   		stop: function( event, ui ) {
-  			console.log();
-  			$.ajax({
-  				url:"http://megabox/switch.html?device=" + event.target.attributes.getNamedItem("device").value + "&cmd=" + event.target.value,
-  				success:function(result){
-  					setTimeout(function() { Refresh(); }, 1000);
-  			}});
-  		}
+  			var socketSwitch = new WebSocket("ws://megabox:3000");
+			socketSwitch.onopen = function(){
+   				socketSwitch.send(event.target.attributes.getNamedItem("device").value + ":" + event.target.value);
+  			} 	
+		}
 	});
 
 
-	$("#refresh").click(function() { Refresh(); });
-
-	Refresh();
-
-	function Refresh() {
-		$.ajax({url:"http://megabox/switch.html?cmd=status",
-			success:function(result){
-				var statuses = JSON.parse(result);
-				$('select').each(function(device) { 
-					if (statuses[$(this)[0].attributes.getNamedItem("device").value]) {
-						$(this).val(statuses[$(this)[0].attributes.getNamedItem("device").value]);
-						$(this).slider('refresh');
-						$(this).slider({ disabled: false });
-					} else {
-						$(this).slider({ disabled: true });
-					}
-				})
-			},
-			error:function(){
-				$('select').each(function(device) { 
-					$(this).slider({ disabled: true });
-				})
-			}
-  		});
-	}
 });
