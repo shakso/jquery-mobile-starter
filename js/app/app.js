779ff7a615e55ce46f95b4ad10fe2b84844388a1
Alex Shakespeare <alex@shakso.com>
2014-11-10 15:13:32 +0000

    hh

--- js/app/app.js
+++ js/app/app.js
@@ -42,8 +42,20 @@ $(function() {
 	});
 
 	$("#quit").click(function() { 
-		navigator.app.exitApp();
-    });
+        navigator.notification.confirm(
+            'Do you really want to exit?',  // message
+            exitFromApp,              // callback to invoke with index of button pressed
+            'Exit',            // title
+            'Cancel,OK'         // buttonLabels
+        );
+    } );
+ 
+    
+    function exitFromApp(buttonIndex) {
+      if (buttonIndex==2){
+       navigator.app.exitApp();
+    	}
+    };
 
 
 });
